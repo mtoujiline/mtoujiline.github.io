0fa533d3ffb669e7ebfb03833d23ddecbd4987f6
Mark Toujiline <mtoujili@calpoly.edu>
2017-06-22 23:09:35 -0700

    Refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
- var x = 0;
- 
- $(document).ready(function(){
-	$('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
-        $(".side-nav-closed").toggleClass('side-nav-open');
-        x=0;
-	});
+(function () {
+    'use strict';
+    var x = 0;
 
-});
+    $(document).ready(function () {
+        $('#nav-icon').click(function () {
+            $(this).toggleClass('open');
+            $(".side-nav-closed").toggleClass('side-nav-open');
+            x = 0;
+        });
+    });
 
-/* Remove side menu on click in main menu */
- $(document).ready(function(){
-	$('section').click(function(){
-        $("#nav-icon3").removeClass('open');
-        $(".side-nav-closed").removeClass('side-nav-open');
-        x=0;
-	});
-    
-});
+    /* Remove side menu on click in main menu */
+    $(document).ready(function () {
+        $('section').click(function () {
+            $("#nav-icon").removeClass('open');
+            $(".side-nav-closed").removeClass('side-nav-open');
+            x = 0;
+        });
+    });
 
-/* Remove side menu on scroll */
-$(document).on( 'scroll', function(){
-    x++;
-    if (x > 30) {
-        $("#nav-icon3").removeClass('open');
-        $(".side-nav-closed").removeClass('side-nav-open');
-        x=0;
-    }
-	
-});
+    /* Remove side menu on scroll */
+    $(document).on('scroll', function () {
+        x = x + 1;
+        if (x > 30) {
+            $("#nav-icon").removeClass('open');
+            $(".side-nav-closed").removeClass('side-nav-open');
+            x = 0;
+        }
+    });
+}());
