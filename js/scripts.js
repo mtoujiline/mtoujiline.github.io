15253cafff62f96646f9f4730baf6730bbd1f7f3
Mark Toujiline <mtoujili@calpoly.edu>
2017-06-22 21:26:56 -0700

    Started using sass/scss, some small navbar changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+ var x = 0;
+ 
+ $(document).ready(function(){
+	$('#nav-icon3').click(function(){
 		$(this).toggleClass('open');
+        $(".side-nav-closed").toggleClass('side-nav-open');
+        x=0;
 	});
+
+});
+
+/* Remove side menu on click in main menu */
+ $(document).ready(function(){
+	$('section').click(function(){
+        $("#nav-icon3").removeClass('open');
+        $(".side-nav-closed").removeClass('side-nav-open');
+        x=0;
+	});
+    
+});
+
+/* Remove side menu on scroll */
+$(document).on( 'scroll', function(){
+    x++;
+    if (x > 30) {
+        $("#nav-icon3").removeClass('open');
+        $(".side-nav-closed").removeClass('side-nav-open');
+        x=0;
+    }
+	
 });
