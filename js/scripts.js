d615c108f3c65b69495e2ee3ab8b2148fee3f941
Mark Toujiline <mtoujili@calpoly.edu>
2017-06-23 23:04:38 -0700

    Menu and content (not counting classes and footer) are done

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
-(function () {
-    'use strict';
-    var x = 0;
-
-    $(document).ready(function () {
-        $('#nav-icon').click(function () {
-            $(this).toggleClass('open');
-            $(".side-nav-closed").toggleClass('side-nav-open');
-            x = 0;
-        });
+var x = 0;
+ 
+$(document).ready(function () {
+    $('#nav-icon').click(function () {
+        $(this).toggleClass('open');
+        $(".side-nav-closed").toggleClass('side-nav-open');
+        x = 0;
     });
+});
 
-    /* Remove side menu on click in main menu */
-    $(document).ready(function () {
-        $('section').click(function () {
-            $("#nav-icon").removeClass('open');
-            $(".side-nav-closed").removeClass('side-nav-open');
-            x = 0;
-        });
+/* Remove side menu on click in main menu */
+$(document).ready(function () {
+    console.log("hi");
+    $('section').click(function () {
+        $("#nav-icon").removeClass('open');
+        $(".side-nav-closed").removeClass('side-nav-open');
+        x = 0;
     });
+});
 
-    /* Remove side menu on scroll */
-    $(document).on('scroll', function () {
-        x = x + 1;
-        if (x > 30) {
-            $("#nav-icon").removeClass('open');
-            $(".side-nav-closed").removeClass('side-nav-open');
-            x = 0;
-        }
-    });
-}());
+
+/* Remove side menu on scroll */
+$(document).on('scroll', function () {
+    x = x + 1;
+    if (x > 30) {
+        $("#nav-icon").removeClass('open');
+        $(".side-nav-closed").removeClass('side-nav-open');
+        x = 0;
+    }
+});
